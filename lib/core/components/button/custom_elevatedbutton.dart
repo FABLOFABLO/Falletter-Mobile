9bf53c2966b3b24e238a50b5e17b1575d5c36a5c
정지윤 <163826760+jiyun040@users.noreply.github.com>
2025-05-12 21:10:35 +0900

    버튼 컴포넌트 추가 및 수정 (#2)
    * 버튼 컴포넌트 추가
    
    * 버튼 컴포넌트 수정
    
    * 버튼 컴포넌트 content 수정
    
    * 버튼 컴포넌트 파라미터 네임 수정

---
+++ lib/core/components/button/custom_elevatedbutton.dart
@@ -0,0 +1,62 @@
+import 'package:falletter/core/constants/color.dart';
+import 'package:falletter/core/constants/text_style.dart';
+import 'package:flutter/material.dart';
+
+class CustomElevatedButton extends StatelessWidget {
+  final VoidCallback? onPressed;
+  final double? height;
+  final double? width;
+  final Gradient? gradient;
+  final Color? textColor;
+  final Widget child;
+
+  const CustomElevatedButton({
+    super.key,
+    this.onPressed,
+    this.height,
+    this.width,
+    this.gradient,
+    this.textColor,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isEnabled = onPressed != null;
+
+    return Container(
+      width: width,
+      height: height ?? 52,
+      decoration: BoxDecoration(
+        gradient:
+            isEnabled
+                ? (gradient ??
+                    FalletterGradient.horizontal(FalletterColor.blueGradient))
+                : FalletterGradient.horizontal([
+                  FalletterColor.gray900,
+                  FalletterColor.gray900,
+                ]),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.transparent,
+          shadowColor: Colors.transparent,
+          surfaceTintColor: Colors.transparent,
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        ),
+        child: DefaultTextStyle(
+          style: FalletterTextStyle.button.copyWith(
+            color:
+                isEnabled
+                    ? (textColor ?? FalletterColor.middleBlack)
+                    : FalletterColor.gray900,
+          ),
+          textAlign: TextAlign.center,
+          child: child,
+        ),
+      ),
+    );
+  }
+}
