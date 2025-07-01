e570b1d227ec69451d26e1c84916f40ca68c5e3f
정지윤 <163826760+jiyun040@users.noreply.github.com>
2025-07-01 12:19:22 +0900

    modal 컴포넌트 구현

---
+++ lib/core/components/modal/default_modal.dart
@@ -0,0 +1,87 @@
+import 'package:falletter/core/components/button/elevated_button.dart';
+import 'package:falletter/core/constants/color.dart';
+import 'package:falletter/core/constants/text_style.dart';
+import 'package:flutter/material.dart';
+
+class DefaultModal extends StatelessWidget {
+  final String title;
+  final String description;
+  final String leftText;
+  final String rightText;
+  final VoidCallback onLeftPressed;
+  final VoidCallback onRightPressed;
+
+  const DefaultModal({
+    super.key,
+    required this.title,
+    required this.description,
+    required this.leftText,
+    required this.rightText,
+    required this.onLeftPressed,
+    required this.onRightPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      backgroundColor: FalletterColor.white,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      insetPadding: const EdgeInsets.all(20),
+      child: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: FalletterTextStyle.subTitle2.copyWith(
+                color: FalletterColor.gray900,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Text(
+                description,
+                style: FalletterTextStyle.body3.copyWith(
+                  color: FalletterColor.gray800,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: CustomElevatedButton(
+                        onPressed: onLeftPressed,
+                        gradient: const LinearGradient(
+                          colors: [FalletterColor.gray200, FalletterColor.gray200],
+                        ),
+                        child: Text(leftText),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: CustomElevatedButton(
+                        onPressed: onRightPressed,
+                        gradient: const LinearGradient(
+                          colors: [FalletterColor.error, FalletterColor.error],
+                        ),
+                        child: Text(rightText),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
