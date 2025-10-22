ddc3fe022e51fa0b10c9623fc87d1bdc37d1a48e
정지윤 <163826760+jiyun040@users.noreply.github.com>
2025-10-22 20:26:28 +0900

    TimerView 추가 (#29)
    다음 질문까지의 대기 화면인 TimerVIew를 따로 분리했습니다.

---
+++ lib/presentation/answer_page/view/timer_view.dart
@@ -0,0 +1,75 @@
+import 'package:falletter/presentation/answer_page/components/circle_progress.dart';
+import 'package:flutter/material.dart';
+import 'package:falletter/core/constants/color.dart';
+import 'package:falletter/core/constants/text_style.dart';
+
+class TimerView extends StatelessWidget {
+  final Duration countdown;
+  final Duration initialCountdown;
+
+  const TimerView({
+    super.key,
+    required this.countdown,
+    required this.initialCountdown,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final minutes = countdown.inMinutes.remainder(60).toString().padLeft(2, '0');
+    final hours = countdown.inHours.toString().padLeft(2, '0');
+    final progress = initialCountdown.inSeconds > 0
+        ? countdown.inSeconds / initialCountdown.inSeconds
+        : 0.0;
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('다음 질문까지 남은 시간',
+              style: FalletterTextStyle.button.copyWith(
+                color: FalletterColor.gray200,
+              )),
+          const SizedBox(height: 20),
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                width: 200,
+                height: 200,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: FalletterColor.middleBlack,
+                ),
+              ),
+              CustomPaint(
+                size: const Size(200, 200),
+                painter: CircleProgress(
+                  progress: progress,
+                  strokeWidth: 10,
+                  gradient: FalletterGradient.vertical(
+                    FalletterColor.blueGradient,
+                  ),
+                ),
+              ),
+              Container(
+                width: 190,
+                height: 190,
+                decoration: const BoxDecoration(
+                  color: FalletterColor.middleBlack,
+                  shape: BoxShape.circle,
+                ),
+                alignment: Alignment.center,
+                child: Text(
+                  '$hours:$minutes',
+                  style: FalletterTextStyle.title1.copyWith(
+                    color: FalletterColor.gray50,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
