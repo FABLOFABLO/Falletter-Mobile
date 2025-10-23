4e5a1fc26e36642cc5682cb71e40021b773d53a9
정지윤 <163826760+jiyun040@users.noreply.github.com>
2025-10-23 20:06:46 +0900

    UserProvider 수정 (#37)
    출석 일수도 표시할 수 있도록 수정했습니다.

--- lib/core/providers/user_provider.dart
+++ lib/core/providers/user_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/legacy.dart';
 
 final currentUserIdProvider = StateProvider<int>((ref) => 1);
-
 final currentUserNicknameProvider = StateProvider<String>((ref) => '유저');
+
+final currentUserAttendanceProvider = StateProvider<int>((ref) => 0);
