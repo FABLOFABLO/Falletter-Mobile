9eab3527b8ebc2f2e385c8136e5f696d6c5d227f
정지윤 <163826760+jiyun040@users.noreply.github.com>
2025-10-23 20:14:06 +0900

    DetailBox, ItemBox 추가 (#35)
    * DetailBox, ItemBox 추가
    
    브릭과 레터가 보이는 부분, 다른 뷰로 이동하는 부분의 컴포넌트를 구현하였습니다.
    
    * DetailBox 수정
    
    시각적 효과가 필요 없기 때문에 Material을 삭제했습니다.

---
+++ lib/presentation/my_page/components/detail_box.dart
@@ -0,0 +1,33 @@
+import 'package:falletter/core/constants/color.dart';
+import 'package:falletter/core/constants/text_style.dart';
+import 'package:flutter/material.dart';
+
+class DetailBox extends StatelessWidget {
+  final Widget child;
+  final void Function() onTap;
+
+  const DetailBox({
+    super.key,
+    required this.child,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: onTap,
+        child: Container(
+          width: double.infinity,
+          height: 60,
+          alignment: Alignment.centerLeft,
+          padding: const EdgeInsets.all(20),
+          child: DefaultTextStyle(
+            style: FalletterTextStyle.button.copyWith(
+              color: FalletterColor.white,
+            ),
+            child: child,
+          ),
+        ),
+      );
+  }
+}
