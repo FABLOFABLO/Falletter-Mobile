e32096713766c619860ce279fd80d3a10e3f7127
정지윤 <163826760+jiyun040@users.noreply.github.com>
2025-10-27 20:02:52 +0900

    Conflict 해결 (#46)
    * Mypage 뷰 구현 (#33)
    
    * Mypage 뷰 구현
    
    * UserProvider와 변수명 통일
    
    * SentLetterView 구현 (#43)
    
    * SentLetterView 추가
    
    내가 보낸 레터 뷰를 구현하였고, 필요한 Provider의 예시값들을 추가했습니다.
    
    * SentTimeUtils 추가
    
    * Update sent_letter_provider.dart
    
    * setState 삭제 및 Provider사용 수정

--- lib/presentation/my_page/views/mypage_view.dart
+++ lib/presentation/my_page/views/mypage_view.dart
@@ -3,11 +3,11 @@ import 'package:falletter/core/constants/color.dart';
 import 'package:falletter/core/constants/text_style.dart';
 import 'package:falletter/core/providers/user_provider.dart';
 import 'package:falletter/presentation/my_page/components/item_box.dart';
+import 'package:falletter/presentation/my_page/widget/title_section.dart';
 import 'package:falletter/presentation/my_page/views/reveive_letter_view.dart';
 import 'package:falletter/presentation/my_page/views/sent_letter_view.dart';
 import 'package:falletter/presentation/my_page/views/theme_view.dart';
 import 'package:falletter/presentation/my_page/views/used_brick_view.dart';
-import 'package:falletter/presentation/my_page/widget/title_section.dart';
 import 'package:falletter/presentation/splash/view/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -16,10 +16,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 class MypageView extends ConsumerWidget {
   final Gradient? gradient;
 
-  const MypageView({
-    super.key,
-    this.gradient,
-  });
+  const MypageView({super.key, this.gradient});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -30,28 +27,23 @@ class MypageView extends ConsumerWidget {
       showDialog(
         context: dialogContext,
         barrierDismissible: false,
-        builder:
-            (BuildContext context) => DefaultModal(
-              title: '로그아웃',
-              description:
-                  '기기내 계정에서 로그아웃 할 수 있어요.\n'
-                  '다음 이용 시에는 다시 로그인 해야합니다.\n'
-                  '정말 로그아웃하시겠어요?',
-              leftText: '취소',
-              rightText: '로그아웃',
-              onLeftPressed: () => Navigator.of(context).pop(),
-              onRightPressed: () {
-                Navigator.of(context).pop();
-
-                ref.read(currentUserNicknameProvider.notifier).state = '';
-                ref.read(currentUserAttendanceProvider.notifier).state = 0;
-
-                Navigator.of(context).pushAndRemoveUntil(
-                  MaterialPageRoute(builder: (context) => const SplashPage()),
+        builder: (BuildContext context) => DefaultModal(
+          title: '로그아웃',
+          description:
+          '기기내 계정에서 로그아웃 할 수 있어요.\n다음 이용 시에는 다시 로그인 해야합니다.\n정말 로그아웃하시겠어요?',
+          leftText: '취소',
+          rightText: '로그아웃',
+          onLeftPressed: () => Navigator.of(context).pop(),
+          onRightPressed: () {
+            Navigator.of(context).pop();
+            ref.read(currentUserNicknameProvider.notifier).state = '';
+            ref.read(currentUserAttendanceProvider.notifier).state = 0;
+            Navigator.of(context).pushAndRemoveUntil(
+              MaterialPageRoute(builder: (context) => const SplashPage()),
                   (route) => false,
-                );
-              },
-            ),
+            );
+          },
+        ),
       );
     }
 
@@ -62,10 +54,7 @@ class MypageView extends ConsumerWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const SizedBox(height: 60),
-            _ProfileHeader(
-              nickname: nickname,
-              attendanceDays: attendanceDays,
-            ),
+            _ProfileHeader(nickname: nickname, attendanceDays: attendanceDays),
             Row(
               children: [
                 Expanded(
@@ -95,28 +84,25 @@ class MypageView extends ConsumerWidget {
               title: '내역',
               items: ['보낸 레터', '받은 레터', '브릭 사용 내역'],
               onTaps: [
-                () {
+                    () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => const SentLetterView(),
-                    ),
+                        builder: (context) => const SentLetterView()),
                   );
                 },
-                () {
+                    () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => const ReveiveLetterView(),
-                    ),
+                        builder: (context) => const ReveiveLetterView()),
                   );
                 },
-                () {
+                    () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => const UsedBrickView(),
-                    ),
+                        builder: (context) => const UsedBrickView()),
                   );
                 },
               ],
@@ -125,12 +111,10 @@ class MypageView extends ConsumerWidget {
               title: '시스템',
               items: ['테마 설정'],
               onTaps: [
-                () {
+                    () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(
-                        builder: (context) => const ThemeView(),
-                    ),
+                    MaterialPageRoute(builder: (context) => const ThemeView()),
                   );
                 },
               ],
@@ -139,7 +123,7 @@ class MypageView extends ConsumerWidget {
               title: '계정',
               items: ['로그아웃'],
               onTaps: [
-                () {
+                    () {
                   _showLogoutConfirmDialog(context);
                 },
               ],
@@ -162,54 +146,44 @@ class _ProfileHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16),
-          child: Container(
-            width: double.infinity,
-            height: 84,
-            decoration: BoxDecoration(
-              color: FalletterColor.middleBlack,
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      gradient: FalletterGradient.vertical(
-                        FalletterColor.blueGradient,
-                      ),
-                      shape: BoxShape.circle,
-                    ),
-                    width: 52,
-                    height: 52,
-                  ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: Container(
+        width: double.infinity,
+        height: 84,
+        decoration: BoxDecoration(
+          color: FalletterColor.middleBlack,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient:
+                  FalletterGradient.vertical(FalletterColor.blueGradient),
+                  shape: BoxShape.circle,
                 ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      nickname,
-                      style: FalletterTextStyle.title3,
-                    ),
-                    Text(
-                      '$attendanceDays일 연속 출석중',
-                      style: FalletterTextStyle.body3.copyWith(
-                        color: FalletterColor.gray400,
-                      ),
-                    ),
-                  ],
+                width: 52,
+                height: 52,
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(nickname, style: FalletterTextStyle.title3),
+                Text(
+                  '$attendanceDays일 연속 출석중',
+                  style: FalletterTextStyle.body3
+                      .copyWith(color: FalletterColor.gray400),
                 ),
               ],
             ),
-          ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
