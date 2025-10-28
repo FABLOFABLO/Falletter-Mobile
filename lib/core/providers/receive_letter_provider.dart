0080a1ec9ad1d5330dbfd33a5b57f8de60ba96a3
정지윤 <163826760+jiyun040@users.noreply.github.com>
2025-10-28 14:29:05 +0900

    LetterBox 컴포넌트 수정 및 ReceivedLetterView 추가

---
+++ lib/core/providers/receive_letter_provider.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_riverpod/legacy.dart';
+
+// 받은 레터 데이터 모델 예시
+class ReceivedLetter {
+  final String reciptionInfo;
+  final DateTime receivedAt;
+  final String content;
+
+  ReceivedLetter({
+    required this.reciptionInfo,
+    required this.receivedAt,
+    required this.content,
+  });
+}
+
+// 더미 데이터
+final List<ReceivedLetter> _initialReceivedLetters = [
+  ReceivedLetter(
+    reciptionInfo: '2114정지윤',
+    receivedAt: DateTime.now().subtract(const Duration(days: 1)),
+    content: '안녕하세요! 다름이 아니라 저번에 발표했던 자료에 대해서 질문이 있어서 연락드렸습니다. 안녕하세요! 다름이 아니라 저번에 발표했던 자료에 대해서 질문이 있어서 연락드렸습니다. 안녕하세요! 다름이 아니라 저번에 발표했던 자료에 대해서 질문이 있어서 연락드렸습니다. 안녕하세요! 다름이 아니라 저번에 발표했던 자료에 대해서 질문이 있어서 연락드렸습니다. ',
+  ),
+  ReceivedLetter(
+    reciptionInfo: '2114정지윤',
+    receivedAt: DateTime.now().subtract(const Duration(hours: 10)),
+    content: '요즘 날씨가 많이 쌀쌀해졌는데, 감기 조심하세요! 요즘 날씨가 많이 쌀쌀해졌는데, 감기 조심하세요! 요즘 날씨가 많이 쌀쌀해졌는데, 감기 조심하세요! 요즘 날씨가 많이 쌀쌀해졌는데, 감기 조심하세요! ',
+  ),
+  ReceivedLetter(
+    reciptionInfo: '2114정지윤',
+    receivedAt: DateTime.now().subtract(const Duration(minutes: 30)),
+    content: '안녕하세요 잘 지내시요? 안녕하세요 잘 지내시요? 안녕하세요 잘 지내시요? 안녕하세요 잘 지내시요? 안녕하세요 잘 지내시요? 안녕하세요 잘 지내시요? 안녕하세요 잘 지내시요? 안녕하세요 잘 지내시요? 안녕하세요 잘 지내시요? 안녕하세요 잘 지내시요? 안녕하세요 잘 지내시요? 안녕하세요 잘 지내시요? 안녕하세요 잘 지내시요? 안녕하세요 잘 지내시요? 안녕하세요 잘 지내시요? 안녕하세요 잘 지내시요? 안녕하세요 잘 지내시요? ',
+  ),
+];
+
+final receivedLettersProvider = StateProvider<List<ReceivedLetter>>((ref) {
+  // 서버에서 데이터를 가져오거나 로컬 DB를 사용
+  return _initialReceivedLetters;
+});
---
+++ lib/presentation/my_page/components/reveived_letter_box.dart
@@ -0,0 +1,35 @@
+import 'package:falletter/core/constants/text_style.dart';
+import 'package:falletter/presentation/my_page/components/letter_box.dart';
+import 'package:flutter/material.dart';
+
+class ReveivedLetterBox extends StatelessWidget {
+  final String arrivedAt;
+  final String preview;
+
+  const ReveivedLetterBox({
+    super.key,
+    required this.arrivedAt,
+    required this.preview,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ReusableLetterBox(
+      children: [
+        Text(
+          arrivedAt,
+          style: FalletterTextStyle.subTitle2,
+        ),
+        const SizedBox(
+          height: 4,
+        ),
+        Text(
+          preview,
+          style: FalletterTextStyle.body3,
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+        ),
+      ],
+    );
+  }
+}
