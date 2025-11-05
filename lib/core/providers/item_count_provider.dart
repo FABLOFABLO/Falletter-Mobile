351de133e53d9021b906d3b83c3dc48543287db1
정지윤 <163826760+jiyun040@users.noreply.github.com>
2025-11-05 13:43:17 +0900

    Riverpod version 변경 반영

--- lib/core/providers/item_count_provider.dart
+++ lib/core/providers/item_count_provider.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
-import 'package:flutter_riverpod/legacy.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 
 final itemCountProvider =
     StateNotifierProvider<itemCountNotifier, Map<String, int>>(
