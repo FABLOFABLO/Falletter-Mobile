b60afa817d65112d2588102fadafca5bfe1432f3
정지윤 <163826760+jiyun040@users.noreply.github.com>
2025-11-06 09:52:26 +0900

    Dio 기반 API 연동 환경 및 endpoint class 추가

---
+++ lib/services/dio.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart';
+import 'package:falletter/core/constants/api_endpoints.dart';
+
+class DioClient {
+  static final DioClient _instance = DioClient._internal();
+  late Dio dio;
+
+  factory DioClient() {
+    return _instance;
+  }
+
+  DioClient._internal() {
+    dio = Dio(
+      BaseOptions(
+        baseUrl: ApiEndPoints.baseUrl,
+      ),
+    );
+  }
+}
