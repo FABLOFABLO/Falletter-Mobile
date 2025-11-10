8d5ec584a008e87abbd5a89745db9aa171d47460
정지윤 <163826760+jiyun040@users.noreply.github.com>
2025-11-10 23:32:44 +0900

    .

---
+++ lib/core/constants/api_endpoints.dart
@@ -0,0 +1,14 @@
+class ApiEndPoints {
+  static const baseUrl = "http://3.34.76.83:8080";
+
+  static const signUp = "/user/signup";
+  static const signIn = "/user/signin";
+  static const logOut = "/user/logout";
+  static const users = "/user/users";
+
+  static const emailVerify = "/auth/email/verify";
+  static const emailMatch = "/auth/email/match";
+
+  static const letterCount = "/item/letter/count";
+  static const brickCount = "/item/brick/count";
+}
