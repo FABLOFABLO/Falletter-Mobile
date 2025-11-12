9be18ea249ac14c655f8e04fbbd12905bcefddcc
정지윤 <163826760+jiyun040@users.noreply.github.com>
2025-11-12 23:20:36 +0900

    Create nickname_provider.dart

---
+++ lib/core/providers/nickname_provider.dart
@@ -0,0 +1,33 @@
+import 'dart:math';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:falletter/core/constants/nonymous_nicknames.dart';
+
+final nicknameProvider = StateNotifierProvider<NicknameNotifier, Map<int, Map<String, String>>>(
+      (ref) => NicknameNotifier(),
+);
+
+class NicknameNotifier extends StateNotifier<Map<int, Map<String, String>>> {
+  NicknameNotifier() : super({});
+  final _random = Random();
+
+  String getOrCreateNickname(int postId, String username) {
+    final postNickMap = state[postId] ?? <String, String>{};
+    if (postNickMap.containsKey(username)) return postNickMap[username]!;
+
+    final nickname = anonymousNicknames[_random.nextInt(anonymousNicknames.length)];
+    state = {
+      ...state,
+      postId: {
+        ...postNickMap,
+        username: nickname,
+      },
+    };
+    return nickname;
+  }
+
+  void clearPostNicknames(int postId) {
+    final newState = Map<int, Map<String, String>>.from(state);
+    newState.remove(postId);
+    state = newState;
+  }
+}
